341a9ee51d9735e4cd41e451a35374d4d686a36c
Shiwakant17 <shiwakantsundaram@gmal.com>
2026-03-15 17:28:27 +0530

    added navigation history panel

--- src/controller/LoggedInUserController.java
+++ src/controller/LoggedInUserController.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.util.Scanner;
 
+import data.Nav;
 import model.Greetings;
 import model.User;
 import service.AdminService;
@@ -16,8 +17,7 @@ public class LoggedInUserController {
     Greetings greetings = new Greetings();
     AdminService adminService = new AdminService();
     LoggedInUserService loggedInUserService = new LoggedInUserService();
-    UserService userService =  new UserService();
-
+    UserService userService = new UserService();
 
     public void loggedInUser(User user) throws InterruptedException, IOException {
         while (true) {
@@ -26,25 +26,37 @@ public class LoggedInUserController {
             int input = sc.nextInt();
             switch (input) {
                 case 1:
-                    loggedInUserService.playSongs();
+                    Nav.log(user.getUserId(), "Opened Play Songs");
+                    loggedInUserService.playSongs(user);
                     break;
 
                 case 2:
-                    adminService.searchSong();
+                    Nav.log(user.getUserId(), "Opened Search");
+                    adminService.searchSong(user.getUserId());
                     break;
+
                 case 3:
+                    Nav.log(user.getUserId(), "Viewed Trending Songs");
                     userService.trendingTop10Songs();
                     break;
 
                 case 4:
+                    Nav.log(user.getUserId(), "Viewed All Songs");
                     adminService.showSongs();
                     break;
 
                 case 5:
+                    Nav.log(user.getUserId(), "Added Song to Playlist");
                     loggedInUserService.appendSongToPlayList(user);
                     break;
 
+                case 6:
+                    Nav.display();
+                    break;
+
                 default:
+                    Nav.log(user.getUserId(), "Logged out");
+                    Nav.clear();
                     return;
             }
         }
